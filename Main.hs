663fd8314f92e89e33cdf4ac7789ea3ad9abe946
szymon <szpawlak@gmail.com>
2018-03-27 23:53:12 +0200

    Adding menu to choose mode of simulation

--- Main.hs
+++ Main.hs
@@ -9,13 +9,20 @@ import System.Posix.Unistd
 
 main = do
     clearScreen
+    putStr "choose option:\nM - manualy trigger next step\nA - automatically go to next step\n"
+    option <- getLine
+    clearScreen
     file <- openFile "./resources/board.txt" ReadMode
     content <- hGetContents file
     let l = lines content
     mapM_ print l
     hClose file
     let loop list = do
-        enter <- getLine
+        if option == "M"
+            then do
+                getLine
+                usleep 0
+            else usleep 200000
         clearScreen
         let new = [applyRuleForlLine board values | board <- list | values <- getSums list]
         mapM_ print new
